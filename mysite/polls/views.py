9850356ffc8c83d31a347de19dd353e87e908daf
Ssingh5 <shalini.singh@morningstar.com>
2020-02-13 16:52:15 +0530

    first commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+from django.shortcuts import render
+from .models import Question
+
+def about(request):
+    return render(request,'about.html')
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+# Create your views here.
